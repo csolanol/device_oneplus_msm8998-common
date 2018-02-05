65ee06a236ed694d70aa12a1d98ddd2da9e59b21
Ralf Luther <luther.ralf@gmail.com>
2018-02-05 20:16:45 +0100

    msm8998: Add flag for persistent HAL3
    forwardport from n7.1
    (plus untested setting for using HAL1 on Skype)
    
    Change-Id: I92d54a0a0302ad420bd8aee7f3ce4ad36aec3848

--- system_prop.mk
+++ system_prop.mk
@@ -72,7 +72,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.oneplus.camera \
-    persist.ts.rtmakeup=false
+    persist.ts.rtmakeup=false \
+    persist.camera.HAL3.enabled=1 \
+    camera.hal1.packagelist=com.skype.raider
 
 # CNE
 PRODUCT_PROPERTY_OVERRIDES += \
