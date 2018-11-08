59eca086ccd83a06e4d99e75d093d5f45df6d641
Ali B <abittin@gmail.com>
2018-11-08 09:57:40 +0300

    build Snapdragon camera
    Change-Id: I52c8e6fb408d91aff7af3037313c0ab5ba26af61

--- common.mk
+++ common.mk
@@ -160,7 +160,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
-    Snap
+    SnapdragonCamera
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
