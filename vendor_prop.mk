c3ca501bd14e6e3a0149d5fb54b0a07f292ac2bb
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-02-25 17:58:19 +0100

    msm8998-common: build versioned vndk
    Change-Id: I0a3d40619819e45f8193d20a632fe36714f3d057

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -343,6 +343,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.vt_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1
 
+# VNDK
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.vndk.version=28
+
 # Wi-Fi
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0
