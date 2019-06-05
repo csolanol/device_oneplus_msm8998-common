ee1856927428364e778224712867eba85e5ad6f1
Ali B <abittin@gmail.com>
2019-06-05 21:30:16 +0300

    msm8998: doze: Add double tap to trigger
    Change-Id: Ieb7d5bd2c0163ea9c3b481f1b894b03867fa74a3

--- doze/Android.mk
+++ doze/Android.mk
@@ -18,7 +18,9 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v7-recyclerview \
     android-support-v7-preference \
     android-support-v7-appcompat \
-    android-support-v14-preference
+    android-support-v14-preference \
+    AicpGear-preference \
+    AicpGear-util
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
