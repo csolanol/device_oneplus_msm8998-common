7591afec65d607ce0eeace19eb5124e8515fb7af
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-08-09 17:33:35 +0200

    msm8998-common: update vendor security patch level from 9.0.8
    Change-Id: Ia2469f80be53b4756b0783e147440a2047a3152e

--- common.mk
+++ common.mk
@@ -447,7 +447,7 @@ PRODUCT_PACKAGES += \
 
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.aicp.build.vendor_security_patch=2019-06-01
+    ro.aicp.build.vendor_security_patch=2019-08-01
 
 # Verity
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/bootdevice/by-name/system
