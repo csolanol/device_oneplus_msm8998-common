a45120ef4b1b20529cb5eca690c7b3b0818957ae
Ali B <abittin@gmail.com>
2019-10-01 19:05:33 +0300

    msm8998-common: shorten aux package list
    Apparently we are limited to 91 bytes
    
    Change-Id: I6e90176d0c650d2e41d8c3845bdc4c0838dd2097

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -103,7 +103,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.oneplus.camera,org.lineageos.snap,com.google.android.Pixel2ModUrnyx05 \
+    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.oneplus.camera,org.lineageos.snap \
     vendor.camera.hal1.packagelist=com.whatsapp \
     persist.camera.mobicat=2 \
     persist.camera.stats.debugexif=3080192 \
