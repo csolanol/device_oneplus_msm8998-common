62b83b16faf9f85492874df308755010bbcc2747
Ali B <abittin@gmail.com>
2019-12-21 14:51:39 +0300

    msm8998-common: use common dalvik-heap device-configs
    Change-Id: I72d08e82467339d2002a445474a4c968f921ddac

--- common.mk
+++ common.mk
@@ -32,13 +32,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=4m \
-    dalvik.vm.heapmaxfree=8m
+# setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+#PRODUCT_PROPERTY_OVERRIDES += \
+#    dalvik.vm.heapstartsize=16m \
+#    dalvik.vm.heapgrowthlimit=256m \
+#    dalvik.vm.heapsize=512m \
+#    dalvik.vm.heaptargetutilization=0.75 \
+#    dalvik.vm.heapminfree=4m \
+#    dalvik.vm.heapmaxfree=8m
 
 # Permissions
 PRODUCT_COPY_FILES += \
