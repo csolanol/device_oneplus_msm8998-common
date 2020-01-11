80ee8c7ef57938ed22b7c99ecc6bb9b091ac2305
Pranav Vashi <neobuddy89@gmail.com>
2020-01-12 00:37:57 +0530

    msm8998-common: Skip interfaces that are buildable
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I24cf761e50853cc1fce2cc1a060c571625369189

--- common.mk
+++ common.mk
@@ -163,8 +163,7 @@ PRODUCT_PACKAGES += \
     Snap
 
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor
+    vendor.qti.hardware.camera.device@1.0
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
@@ -196,8 +195,7 @@ PRODUCT_PACKAGES += \
     libvulkan
 
 PRODUCT_PACKAGES += \
-    vendor.display.config@1.0 \
-    vendor.display.config@1.0_vendor
+    vendor.display.config@1.0
 
 # Doze
 PRODUCT_PACKAGES += \
