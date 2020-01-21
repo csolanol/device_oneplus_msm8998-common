crdroidandroid/android_device_oneplus_msm8998-common

msm8998-common: Update vendor security date from 9.0.10
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

common.mk
@@ -456,7 +456,7 @@ PRODUCT_PACKAGES += \
 
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.lineage.build.vendor_security_patch=2019-10-01
+    ro.lineage.build.vendor_security_patch=2019-12-01
 
 # Verity
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/bootdevice/by-name/system

csolanol/device_oneplus_msm8998-common

Change-Id: If0fc606be9eb020276d163aab284285f043a24a0

common.mk
@@ -474,7 +474,7 @@ PRODUCT_PACKAGES += \
 
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.aicp.build.vendor_security_patch=2019-10-01
+    ro.aicp.build.vendor_security_patch=2019-12-01
 
 # Verity
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/bootdevice/by-name/system