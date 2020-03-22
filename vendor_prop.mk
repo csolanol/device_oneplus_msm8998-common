33d5bff29fbb340d3ad739ca86f3e2177b591e28
Pranav Vashi <neobuddy89@gmail.com>
2020-03-22 13:34:40 +0530

    msm8998-common: Update few props from Android 10
    * Hydrogen Leaked Beta
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I84a1052c9a6cf8f2c61c61ab65c88159f04d24fb

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -193,6 +193,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.frp.pst=/dev/block/bootdevice/by-name/config
 
+# GPS
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.overlay.izat.optin=rro
+
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.hwui.use_buffer_age=false \
@@ -248,20 +252,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
     mmp.enable.3g2=true \
     media.aac_51_output_enabled=true \
     mm.enable.sec.smoothstreaming=true \
-    vendor.mm.enable.qcom_parser=16760831 \
     persist.mm.enable.prefetch=true \
     vidc.enc.dcvs.extra-buff-count=2 \
     persist.media.treble_omx=true \
     vendor.vidc.debug.level=1 \
     vendor.swvdec.log.level=1 \
-    # 13631487 is decimal sum of supported codecs in AAL
-    # codecs: (PARSER_)AAC AC3 AMR_NB AMR_WB ASF AVI DTS FLV
-    # 3GP 3G2 MKV MP2PS MP2TS MP3 OGG QCP WAV FLAC AIFF APE DSD
-    #mm.enable.qcom_parser=13631487
-    # VENDOR_EDIT Add codecs:DIVX & DIVXHD
-    # if temporary OGG is removed
-    # mm.enable.qcom_parser=16760831
-    mm.enable.qcom_parser=16777215
+    vendor.mm.enable.qcom_parser=67092479 \
+    vendor.vidc.enc.disable.pq=true
 
 # NFC
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -378,6 +375,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.df.iwlan_mux=9 \
     persist.data.df.dev_name=rmnet_usb0 \
     persist.vendor.data.iwlan.enable=true
+
+# Sdcard
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.fuse_sdcard=true
 
 # Sensors
 PRODUCT_PROPERTY_OVERRIDES += \
