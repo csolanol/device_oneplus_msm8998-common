0623caa50697cd40accfe4ec58578d13a53cf14d
Ali B <abittin@gmail.com>
2020-03-22 22:57:35 +0300

    msm8998-common: update device package name
    Change-Id: Iad060d935f22c26b4b3611c6cdffc4db3e60542f

--- common.mk
+++ common.mk
@@ -511,7 +511,7 @@ PRODUCT_PACKAGES += \
     wifi-mac-generator
 
 PRODUCT_PACKAGES += \
-    DeviceParts
+    OnePlusParts
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/fstman.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/fstman.ini \
