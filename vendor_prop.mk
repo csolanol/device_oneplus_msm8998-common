3f763a4a5a13f1f2c881461fcb3f414060652114
Pranav Vashi <neobuddy89@gmail.com>
2020-04-13 19:34:44 +0530

    msm8998-common: Enable value added AOSP features
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ibf6888b08f61ef74bc7af3e690e0ea70834bbcf1

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -444,6 +444,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.vt_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1
 
+# Vendor
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.va_aosp.support=1
+
 # VoLTE
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.ims.dropset_feature=0 \
